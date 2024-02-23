54b0d93e4d0484c1abf9f5785f7269c98f3f2d87
Máté Dombóvári <63262118+Stalker9651@users.noreply.github.com>
2024-02-23 12:02:28 +0100

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Tabor> lista = new List<Tabor>();
+            Console.WriteLine("Hello, World!");
+
+            using (var reader = new StreamReader("taborok.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    lista.Add(new Tabor(reader.ReadLine()));
+                }
+            }
+            //2. feladat
+            Console.WriteLine($"2. feladat\nAz adatsorok száma: {lista.Count}");
+            Console.WriteLine($"Az először rögzített tábor témája: {lista[0].tabortema}");
+            Console.WriteLine($"Az utoljára rögzített tábor témája: {lista[lista.Count-1].tabortema}");
+            //3. feladat
+            Console.WriteLine("3. feladat");
+            var f3 = lista.FindAll(v => v.tabortema == "zenei").ToList();
+            if(f3.Count != 0)
+            {
+                foreach (var item in f3)
+                {
+                    Console.WriteLine($"Zenei tábor kezdődik: {item.kezdeshonap}. hó {item.kezdesnap}. napján");
+                }
+            }
+            // 4.feladat
+            Console.WriteLine("4. feladat");
+            var f4 = lista.MaxBy(v => v.diakbetujel.Count());
+            Console.WriteLine("Legnépszerűbbek: ");
+            foreach (var item in lista)
+            {
+                if(item.diakbetujel.Count() == f4.diakbetujel.Count())
+                {
+                    Console.WriteLine($"{item.kezdeshonap} {item.kezdesnap} {item.tabortema}");
+                }
+
+            }
+
+            // 6.feladat
+            Console.WriteLine("6. feladat");
+            Console.Write("hó:");
+            int honapInput = int.Parse(Console.ReadLine());
+            Console.Write("nap:");
+            int napInput = int.Parse(Console.ReadLine());
+            DateTime taborInput = new DateTime(2020, honapInput, napInput);
+            DateTime nyarKezdet = new DateTime(2020, 6, 16);
+            DateTime nyarVege = new DateTime(2020, 8, 31);
+            var f6 = lista.FindAll(v => v.DateKezdes <= taborInput && v.DateVege >= taborInput).ToList();
+
+            Console.WriteLine($"Ekkor épp {f6.Count} tábor tart.");
+
+            //7.feladat
+            Console.WriteLine("7. feladat");
+            Console.Write("Adja meg egy tanuló betűjelét: ");
+            string diakInput = Console.ReadLine();
+            var f7 = lista.FindAll(lista => lista.diakbetujel.Contains(diakInput)).ToList();
+
+
+            f7 = f7.OrderBy(v => v.kezdeshonap).ThenBy(v => v.kezdesnap).ToList();
+            
+            using(var writer = new StreamWriter("egytanulo.txt")) {
+                foreach (var item in f7)
+                {
+                    writer.WriteLine($"{item.kezdeshonap}.{item.kezdesnap}-{item.vegehonap}.{item.vegenap}. {item.tabortema}");
+
+
+                }
+            }
+            int overlap = 0;
+            for (int i = 0; i < f7.Count; i++)
+            {
+                for (int j = 1; j < f7.Count - i; j++)
+                {
+                    if (f7[i].DateKezdes < f7[j].DateVege && f7[j].DateKezdes < f7[i].DateVege)
+                    {
+                        overlap++;
+                    }
+                }
+
+            }
+            if (overlap > 0)
+            {
+                Console.WriteLine("Nem mehet el mindegyik táborba.");
+            }
+            else
+            {
+                Console.WriteLine("Minden táborba elmehet.");
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+            // 5.feladat
+            int sorszam(int honap, int nap)
+            {
+
+                DateTime nyarKezdet = new DateTime(2020, 6, 16);
+                DateTime nyarVege = new DateTime(2020, 8, 31);
+                DateTime taborKezdet = new DateTime(2020, honap, nap);
+                int napok = (taborKezdet - nyarKezdet).Days;
+                return napok;
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp6/Tabor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Tabor
+    {
+        public int kezdeshonap { get; set; }
+        public int kezdesnap { get; set; }
+        public int vegehonap { get; set; }
+        public int vegenap { get; set; }
+        public string diakbetujel { get; set; }
+        public string tabortema { get; set; }
+
+        public DateTime DateKezdes
+        {
+            get
+            {
+                return new DateTime(2020, kezdeshonap, kezdesnap);
+            }
+        }
+
+        public DateTime DateVege
+        {
+            get
+            {
+                return new DateTime(2020, vegehonap, vegenap);
+            }
+        }
+
+
+
+
+        public Tabor(string src)
+        {
+            var split = src.Split('\t');
+            kezdeshonap = int.Parse(split[0]);
+            kezdesnap = int.Parse(split[1]);
+            vegehonap = int.Parse(split[2]);
+            vegenap = int.Parse(split[3]);
+            diakbetujel = split[4];
+            tabortema = split[5];
+        }
+
+    }
+}
